d57fd6c9581a353850cb203955f534c7304922ad
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-21 23:55:58 +0800

    feat: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#E3F6F5",
+        accent: "#272343",
+        "dark-primary": "#BAE8E8",
+      },
+      textColor: {
+        accent: "#272343",
+      },
+    },
   },
   plugins: [],
 };
