9eeb1b076e46a7375cc606919863118e58fe302e
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-22 16:17:41 +0800

    feat: add randomizing of headings

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface ModalProps {
   content: string;
 }
 
 const Modal: React.FC<ModalProps> = ({ content }) => {
+  const randomHeadings = [
+    "Everything happens for a reason.",
+    "Go for it!",
+    "That's the best choice!",
+    "Just do it!",
+    "That's the best ngl.",
+    "The soul becomes dyed with the color of its thoughts.",
+    "Everything happens when it's supposed to.",
+    "Everything comes gradually at its appointed hour.",
+    "What's meant to be will always find a way.",
+    "Once you make a decision, the universe conspires to make it happen.",
+  ];
+  const [randomHeading, setRandomHeading] = useState("");
+  const hasError = content.includes("Error");
+
+  useEffect(() => {
+    setRandomHeading(
+      randomHeadings[Math.floor(Math.random() * randomHeadings.length)]
+    );
+  }, [content]);
+
   return (
     <>
       <input type="checkbox" id="modal" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Random later</h3>
-          <p className="py-4">{content}</p>
+          <h3 className="font-bold text-lg">
+            {hasError ? "Please follow instructions!" : randomHeading}
+          </h3>
+          <p className="py-4 ">
+            {hasError ? "You got an" : "You got"}{" "}
+            <span className="text-accent font-bold">{content}!</span>
+          </p>
           <div className="modal-action">
             <label htmlFor="modal" className="btn">
               Thanks!
