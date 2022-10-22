52b6c1c25a92deed1d0a6d8fce60fdca81627104
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-22 12:55:05 +0800

    feat: add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return <nav className='h-10 grid items-center text-center bg-dark-primary '><h1>Decisive</h1></nav>;
+};
+
+export default Navbar;
