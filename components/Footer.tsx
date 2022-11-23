11aa101cb2a801d155b9ae0ab0cc3f75e10d7eaf
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-23 22:07:48 +0800

    feat: format code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className='w-full p-3 text-center grid items-center bg-dark-primary mt-auto'>Made by Jiseeeh 🖤</footer>;
+  return (
+    <footer className="w-full p-3 text-center grid items-center bg-dark-primary mt-auto">
+      Made by Jiseeeh 🖤
+    </footer>
+  );
 };
 
 export default Footer;
