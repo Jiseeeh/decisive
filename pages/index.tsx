7f4683213bd2019219f76ffae082b15ad0525d55
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-23 21:45:11 +0800

    refractor: move randomize to a helper function

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Modal from "../components/Modal";
 import randomizeMilliseconds from "../helper/randomizeMilliseconds";
+import randomizeChoice from "../helper/randomizeChoice";
 
 const Home: NextPage = () => {
   const isIdle = useIdle(10000);
@@ -19,21 +20,7 @@ const Home: NextPage = () => {
   };
 
   const onRandomize = (t: Toast) => {
-    const randomChoices: string[] = [
-      "Buy now, Buy later",
-      "Go out alone, Go out with friends",
-      "Workout now, Workout later, Workout tomorrow",
-      "Take a nap, Continue studying",
-      "React, Vue, Svelte, Angular, Solid",
-      "Node, Laravel, SpringBoot",
-      "Cheat meal today, Continue Diet",
-      "Do homework, Play for 1 hour, Eat snacks first",
-      "Play for an hour, Play for two hours, Play for four hours",
-      "Watch an anime series, Watch a KDrama series, Watch an anime movie",
-    ];
-
-    const choice =
-      randomChoices[Math.floor(Math.random() * randomChoices.length)];
+    const choice = randomizeChoice();
 
     setInputValue(choice);
     toast.dismiss(t.id);
